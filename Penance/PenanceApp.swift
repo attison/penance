82e11f612fe9ee7575c8d85edc67c8f8b56c4d62
attison <58448133+attison@users.noreply.github.com>
2026-01-27 14:57:16 -0600

    Initial Commit

---
+++ Penance/PenanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PenanceApp.swift
+//  Penance
+//
+//  Created by Attison Barnes on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct PenanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
